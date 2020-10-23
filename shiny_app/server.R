f45ae1cd8f42bd5b41bc3a975cfb357035f7475e
Winona Guo <winona_guo@college.harvard.edu>
2020-10-23 19:33:58 -0400

    Milestone 4

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -8,18 +8,44 @@ library(readxl)
 colonialism <- read_xls("raw_data/colonialism.xls") %>%
   clean_names() 
 
-# Adding my first graph.
+vulnerability <- read_xls("raw_data/vulnerability.xls", skip = 2) %>%
+  clean_names() %>%
+  rename(vulnerable = climate_vulnerability_cv_cdi_adj_for_income_regulation)
+
+#getwd(), setwd()
 
 shinyServer(function(input, output) {
 
     output$colonialPlot <- renderPlot({
     
-    colonialism %>%
-        filter(country_name %in% c("Afghanistan", "China", "Angola")) %>%
-        ggplot(mapping = aes(x = country_name, y = colyears)) + 
-            geom_col()
+      q1 <- colonialism %>%
+        select(country_name, colyears) 
+      
+      q2 <- vulnerability %>%
+        select(country, vulnerable, world_sub_region)
+      
+      q3 <- inner_join(q1, q2, by = c("country_name" = "country")) 
+      
+      graph1 <- q3 %>%
+        ggplot(aes(x = colyears, y = vulnerable, color = world_sub_region)) +
+        geom_point(alpha = 0.7) +
+        labs(title = "Countries by Years Colonized and Vulnerability to Climate Risk",
+             x = "Years Colonized",
+             y = "Vulnerability to Climate Risk",
+             caption = "This graph indicates there is not a significant correlation
+       overall between the years a country has been colonized, 
+       and its current vulnerability to climate risk.") +
+        theme_bw() + 
+        scale_color_discrete("World Region")
+      
+      graph1
+      
     })
-
+    
+    output$country_message <- renderText({
+      paste0("This is the country you chose: ", 
+             input$selected_country, "!")
 })
-
+    
+})
 
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -9,17 +9,47 @@ library(readxl)
 
 # Only three things go here. 1. Static text like p() and h2(). 2. Inputs like text box and sliders which the user interacts with. 3. Display
 
+country.names <- c("Somalia", "China", "Afghanistan")
+
 shinyUI(
     navbarPage(theme = shinytheme("cyborg"),
                "Climate Colonialism",
                tabPanel(
                    "About",
-                   p("This project will analyze the relationship between empire and climate risk. I will take three separate sets of data: about countries impacted by colonialism, the CO2 emissions of countries, and the projected climate risk of countries. I hope to visualize if there is a correlation between empire and future effects of the climate.")),
-                   h6(a("Here is my first dataset about climate risk.", href = "https://www.cgdev.org/publication/dataset-vulnerability-climate-change")),
+                   p(),
+                   h4("Welcome to my Gov 50 final project! My name is Winona, and I am a junior at Harvard College. 
+                      This semester, I was interested in understanding the relationship between empire and climate risk. 
+                      Over the past few centuries, colonialism has violently affected countries across the world. 
+                      Today, alongside ongoing relationships of dependence and domination, the effects of colonial encounters persist. 
+                      I would like to know if climate risk is among them.
+                      To do so, I will take three separate sets of data: 
+                      on countries in Asia and Africa impacted by colonialism, the CO2 emissions of countries, and the projected climate risk of countries."),
+                   h4(a("This is where I found data about climate risk.", href = "https://www.cgdev.org/publication/dataset-vulnerability-climate-change")),
+                   h4(a("This is where I found data about colonialism in Asia and Africa.", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/UQZFYA"))
+               ),
+              
                tabPanel(
                    "Mapping Global Empires", 
+                   mainPanel(
                    plotOutput("colonialPlot")
-               )
-    ))
+                   )
+                  ),
+               
+               tabPanel(
+                 "Interactive Activity", 
+                 sidebarLayout(
+                   sidebarPanel("Choose a country.",
+                                p("As I continue working on my project, different options you select will go to more information about each country."),
+                                selectInput(inputId = "selected_country",                  # a name for the value you choose here
+                                            label = "Choose a country from this list!",    # the name to display on the slider
+                                            choices = country.names)),
+                   mainPanel(
+                     textOutput("country_message")
+                   )
+                  )
+                )
+          )
+)
+
 
 
