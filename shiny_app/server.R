667028c9bae4e649d34a4360ed4ab943ccef9caa
Winona Guo <winona_guo@college.harvard.edu>
2020-12-02 18:31:11 -0500

    Progress as of afternoon Wed Dec 2.

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,34 +1,96 @@
+#### PREP ####
+
 library(shiny)
 library(shinythemes)
 library(tidyverse)
 library(janitor)
 library(readr)
 library(readxl)
+library(leaflet)
+library(maptools)
+library(rgdal)
+library(dplyr)
+library(RColorBrewer)
 
 colonialism <- read_xls("raw_data/colonialism.xls") %>%
   clean_names()
 
-vulnerability <- read_xls("raw_data/vulnerability.xls", skip = 2) %>%
+climaterisk <- read_xls("raw_data/vulnerability.xls", skip = 2) %>%
   clean_names() %>%
-  rename(vulnerable = climate_vulnerability_cv_cdi_adj_for_income_regulation)
+  rename(vulnerable = climate_vulnerability_cv_cdi_adj_for_income_regulation) %>%
+  select(country, vulnerable, world_sub_region)
 
-#getwd(), setwd()
+countries <- read_xls("raw_data/countries_files/icowcol.xls") %>%
+  mutate(date = str_sub(Indep, 1, 4)) %>%
+  mutate(twenty = ifelse(date >= 1900, "Colonized", "Independent"))
+
+wrldsimplmod <- readRDS("joined.RDS")
+
+fit_mod <- inner_join(countries, climaterisk, by = c("Name" = "country")) %>%
+  group_by(twenty) %>%
+  mutate(avg_risk = mean(vulnerable)) 
+
+emissions <- read_csv("raw_data/emissions.csv", 
+                      col_types = cols(
+                        Year = col_double(),
+                        Country = col_character(),
+                        Total = col_double(),
+                        `Solid Fuel` = col_double(),
+                        `Liquid Fuel` = col_double(),
+                        `Gas Fuel` = col_double(),
+                        Cement = col_double(),
+                        `Gas Flaring` = col_double(),
+                        `Per Capita` = col_double(),
+                        `Bunker fuels (Not in Total)` = col_double()
+                      )) %>%
+  group_by(Country) %>%
+  summarize(sum = sum(Total), .groups = "drop") %>%
+  select(Country, sum) %>%
+  mutate(Country = str_to_title(Country)) 
+
+
+#### SETTING UP SHINY SERVER ####
 
 shinyServer(function(input, output) {
+  
+#### FIRST PAGE ####
 
+  output$countriesPlot <- renderPlot({
+    
+    bargraph <- fit_mod %>%
+      ggplot(mapping = aes(x = twenty, y = avg_risk)) + 
+      geom_col(fill = "darkseagreen4") +
+      theme_bw() +
+      labs(title = "Climate Risk Today for Countries Independent vs. Colonized At The Turn of the 20th Century",
+           x = "Countries (Until Decolonization Post-1900)",
+           y = "Average Climate Vulnerability Today") +
+      theme_classic()
+    
+    bargraph
+    
+  })
+  
+#### SECOND PAGE ####  
+  
     output$colonialPlot <- renderPlot({
     
+      if (input$selected_characteristic == "Years Colonized") ({
+        
+        #display first line plot
+        
       q1 <- colonialism %>%
         select(country_name, colyears) 
       
-      q2 <- vulnerability %>%
-        select(country, vulnerable, world_sub_region)
+      q2 <- climaterisk %>%
+        subset(country != "Somalia") %>%
+        subset(country != "Burundi")
       
       q3 <- inner_join(q1, q2, by = c("country_name" = "country")) 
       
-      graph1 <- q3 %>%
+      colonize <- q3 %>%
         ggplot(aes(x = colyears, y = vulnerable, color = world_sub_region)) +
         geom_point(alpha = 0.7) +
+        geom_smooth(formula = y ~ x, method = "lm", se = FALSE) +
         labs(title = "Countries by Years Colonized and Vulnerability to Climate Risk",
              x = "Years Colonized",
              y = "Vulnerability to Climate Risk",
@@ -38,14 +100,174 @@ shinyServer(function(input, output) {
         theme_bw() + 
         scale_color_discrete("World Region")
       
-      graph1
+      colonize
+      })
       
+    
+    else(input$selected_characteristic == "Type of Colonization") ({
+      
+    types <- fit_mod %>%
+      ggplot(aes(x = Type, y = vulnerable)) +
+      geom_col(fill = "orange") +
+      labs(title = "The Effect of Different Types of Colonialism on Climate Risk",
+           y = "Climate Risk",
+           subtitle = "Key: 1 = Formation, 2 = Decolonization, 3 = Secession, 4 = Partition",
+           caption = "This graph shows that decolonialization and occuption by a foreign power 
+           has a significantly greater effect on risk than histories of secession or partition.") 
+    
+    types
+    
     })
     
-    output$country_message <- renderText({
-      paste0("This is the country you chose: ", 
-             input$selected_country, "!")
-})
+  })
+    
+    output$message <- renderText({
+      paste0("More information: ", 
+             input$selected_characteristic, "!")
+    })
+    
+#### THIRD PAGE ####
+    
+# PART ONE: CLIMATE RISK MAP
+    
+    # Table with names of countries, sorted by climate risk.
+    
+    output$risk <- renderDataTable({
+      datatable(
+        
+        selection <- fit_mod %>%
+          select(Name, date, twenty, vulnerable) %>%
+          rename("Start of 20th Century" = "twenty",
+                 "Year Decolonized" = "date",
+                 "Climate Risk" = "vulnerable",
+                 "Country" = "Name"),
+        options = list(pageLength = 10)
+      )
+    })
+    
+    # Leaflet map, with help referencing Wyatt Hurt's project on Transboundary Water
+    # Conflict.
+    
+    output$map <- renderLeaflet({
+      
+      # Set color palette, using 9 bins.
+      
+      bins_climaterisk <-
+        colorBin(
+          "Blues",
+          fit_mod$vulnerable,
+          9,
+          pretty = FALSE,
+          na.color = "#DFDFDF"
+        )
+      
+      # Build map using CARTO DB Positron provider tiles. View middle of earth.
+      
+      leaflet(width = "100%") %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
+        setView(lng = 0,
+                lat = 30,
+                zoom = 1.5) %>%
+        
+        # Adding polygons.
+        
+        addPolygons(
+          data = wrldsimplmod,
+          stroke = FALSE,
+          smoothFactor = 0.2,
+          fillOpacity = 1,
+          popup = paste(
+            fit_mod$Name,
+            "Country <br>",
+            "Climate Risk:",
+            fit_mod$vulnerable
+          ),
+          color = ~ bins_climaterisk(fit_mod$vulnerable)
+        ) %>%
+        
+        # The map legend. 
+        
+        addLegend(
+          "bottomright",
+          pal = bins_climaterisk,
+          values = fit_mod$vulnerable,
+          title = "Climate Risk",
+          opacity = 1,
+          labFormat = labelFormat(digits = 0)
+        )
+    })
+    
+# PART TWO: EMISSION MAP
+    
+    # Table with names of countries, sorted by climate risk.
+    
+    output$emissions <- renderDataTable({
+      datatable(
+        emissions,
+        options = list(pageLength = 10)
+      )
+    })
+    
+    # Leaflet map
+    
+    output$map_2 <- renderLeaflet({
+      
+      # Set color palette, using 9 bins.
+      
+      bins_emissions <-
+        colorBin(
+          "Reds",
+          emissions$sum,
+          9,
+          pretty = FALSE,
+          na.color = "#DFDFDF"
+        )
+      
+      # Build map using CARTO DB Positron provider tiles. View middle of earth.
+      
+      leaflet(width = "100%") %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
+        setView(lng = 0,
+                lat = 30,
+                zoom = 1.5) %>%
+        
+        # Adding polygons.
+        
+        addPolygons(
+          data = wrldsimplmod,
+          stroke = FALSE,
+          smoothFactor = 0.2,
+          fillOpacity = 1,
+          popup = paste(
+            emissions$Country,
+            "Country <br>",
+            "Emissions:",
+            emissions$sum
+          ),
+          color = ~ bins_emissions(emissions$sum)
+        ) %>%
+        
+        # The map legend. 
+        
+        addLegend(
+          "bottomright",
+          pal = bins_emissions,
+          values = emissions$sum,
+          title = "Total Emissions",
+          opacity = 1,
+          labFormat = labelFormat(digits = 0)
+        )
+    })
+    
+#### FOURTH PAGE ####
+
+# Running my model.
+    
+#### FIFTH PAGE ####
+    
+# This is my About page, and I only have text-based elements!
+    
+#### CLOSING OUT SHINY ####
     
 })
 
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,54 +1,169 @@
+#### PREP ####
+
 library(shiny)
 library(shinythemes)
 library(tidyverse)
 library(janitor)
 library(readr)
 library(readxl)
+library(leaflet)
+library(maptools)
+library(DT)
 
-#Setting up my page structure.
-
-# Only three things go here. 1. Static text like p() and h2(). 2. Inputs like text box and sliders which the user interacts with. 3. Display
-
-country.names <- c("Somalia", "China", "Afghanistan")
+#### SETTING UP THE PAGE ####
 
 shinyUI(
-    navbarPage(theme = shinytheme("cyborg"),
+    navbarPage(theme = shinytheme("journal"),
                "Climate Colonialism",
+
+#### FIRST PAGE ####
+
                tabPanel(
-                   "About",
+                   "The Challenge",
                    p(),
+                   h4("The empire of the climate is the first, the most powerful of all empires —Montesquieu"),
+                   h2(" "),
+               mainPanel(
+                     plotOutput("countriesPlot"),
+                     h4("Why is this the case?")
+                   )    
+               ),
+
+#### SECOND PAGE ####
+
+               tabPanel(
+                 "History of Empire", 
+                 
+                 fluidPage(
+                   
+                   titlePanel("A Short History"),
+                   
+                   p(paste("Here I will include a short historical background on the history of empire, 
+                   and its relationship with climate colonialism.")),
+                   
+                   sidebarLayout(
+                     
+                     # Left panel. Adding more options soon.
+                     
+                     sidebarPanel(
+                       
+                       helpText("Choose a characteristic of colonialism:"),
+                       
+                       selectInput("selected_characteristic", h3("Characteristics"),
+                                   choices = list("Years Colonized",
+                                                  "Type of Colonization")),
+                     ),
+                     
+                    # Right side plot. 
+                    
+                      mainPanel(
+                         plotOutput("colonialPlot"),
+                         textOutput("message")
+                      )
+                     )
+                   
+                   )
+                 ),
+
+#### THIRD PAGE ####
+
+               tabPanel(
+                 "Emissions vs. Effects", 
+
+# PART ONE    
+                fluidPage(
+                  
+                  # Two columns. 
+                  
+                  fluidRow(column(
+                    4,
+                    h3("Countries around the world will be impacted differently by the climate crisis."),
+                    p("Click on the countries to the right for their projected climate risk."
+                    ),
+                    
+                    # Plot table.
+                    
+                    dataTableOutput("risk")
+                  ),
+                  column(8,
+                         
+                         # Plot Leaflet map.
+                         
+                         leafletOutput("map", height = 700))
+                     ),
+# PART TWO                  
+                  fluidRow(column(
+                    4,
+                    h3("But these countries are contributing to the climate crisis at very different rates."),
+                    p("Click to the right for countries' CO2 emissions between 1751 and 2014."
+                    ),
+                    
+                    # Plot table.
+                    
+                    dataTableOutput("emissions")
+                  ),
+                  column(8,
+                         
+                         # Plot Leaflet map.
+                         
+                         leafletOutput("map_2", height = 700))
+                  )
+                  
+                 )
+
+                ),
+              
+#### FOURTH PAGE ####
+
+                tabPanel(
+                  "Model",
+                  h4("Introduce my model.")
+                ),
+
+
+#### FIFTH PAGE ####
+
+               tabPanel(
+                 "About", 
+                 mainPanel(
                    h4("Welcome to my Gov 50 final project! My name is Winona, and I am a junior at Harvard College. 
                       This semester, I was interested in understanding the relationship between empire and climate risk. 
                       Over the past few centuries, colonialism has violently affected countries across the world. 
                       Today, alongside ongoing relationships of dependence and domination, the effects of colonial encounters persist. 
                       I would like to know if climate risk is among them.
-                      To do so, I will take three separate sets of data: 
-                      on countries in Asia and Africa impacted by colonialism, the CO2 emissions of countries, and the projected climate risk of countries."),
-                   h4(a("This is where I found data about climate risk.", href = "https://www.cgdev.org/publication/dataset-vulnerability-climate-change")),
-                   h4(a("This is where I found data about colonialism in Asia and Africa.", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/UQZFYA"))
-               ),
-              
-               tabPanel(
-                   "Mapping Global Empires", 
-                   mainPanel(
-                   plotOutput("colonialPlot")
-                   )
-                  ),
-               
-               tabPanel(
-                 "Interactive Activity", 
-                 sidebarLayout(
-                   sidebarPanel("Choose a country.",
-                                p("As I continue working on my project, different options you select will go to more information about each country."),
-                                selectInput(inputId = "selected_country",                  # a name for the value you choose here
-                                            label = "Choose a country from this list!",    # the name to display on the slider
-                                            choices = country.names)),
-                   mainPanel(
-                     textOutput("country_message")
-                   )
-                  )
-                )
-          )
+                      To do so, I have taken four separate datasets: 
+                        on colonial history, countries in Asia and Africa impacted by colonialism, the CO2 emissions of countries, and the projected climate risk of countries."),
+                   h5("First is a 2007 dataset on colonial history from the Harvard Dataverse. 
+                      This was put together by the Issue Correlates of War (ICOW) Project, 
+                      which tried 'to identify colonial or other dependency relationships for each state over the past two centuries.' 
+                      The dataset includes the years in which countries were decolonized, as well as characteristics like the type of independence, whether it was violent, etc."),
+                   h5(a("ICOW Colonial History Data", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/5EMETG")),
+                   h5("Next is a 2017 dataset from the Harvard Dataverse which measures characteristics of colonialism specifically in countries in Africa and Asia.
+                      I found this useful because it contains a column specifying the number of years countries were colonized (a variable not found in the previously mentioned dataset).
+                      I was interested in whether there is a correlation between number of years colonized and climate risk, so I combined this data with climate vulnerability data for a graph on the second tab.
+                      It is important to note that this dataset does not capture important regions in the world like Latin America and Europe. 
+                      Nevertheless, it is still interesting to see the results in Asia and Africa, since those regions have been heavily affected by colonialism over time."),
+                   h5(a("Colonialism in Asia and Africa Data", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/UQZFYA")),
+                   h5("Third is a 2011 dataset on climate risk, created by David Wheeler for the Center for Global Development. 
+                      This was perhaps the most important dataset for my project, because it 'measures the vulnerability of 233 countries to three major effects of climate change: weather-related disasters, sea-level rise, and reduced agricultural productivity'.
+                      One important note is that this dataset was collected in 2011. In the 9 years which have passed since then, our world and individual countries have changed in their increasing susceptibility to environmental harm. 
+                      I wonder whether a dataset collected today would reflect significantly different comparative risks across countries.
+                      Nevertheless, since this project discusses the effects of the colonial era (especially in the sense of political control, legal status, and physical occupation, all still going strong in the 20th century), perhaps data closer to then is still useful to see."),
+                   h5(a("Climate Risk Data", href = "https://www.cgdev.org/publication/dataset-vulnerability-climate-change")),
+                   h5("Last is a 2013 dataset from the Carbon Dioxide Information Analysis Center (Boden, Marland, Andres). 
+                      It measures CO2 Emissions from fossil-fuels per country annually from 1751 to 2014.
+                      I found this dataset on Github, prepared by the datahub.io project."),
+                   h5(a("CO2 Emissions Data", href = "https://github.com/datasets/co2-fossil-by-nation")),
+                   h4("Finally, I just wanted to sound three notes of caution: First, do not interpret any of the linkages I have created on this site as causal. I am interested in examining whether there is a relationship between different variables as a predictive question.
+                   Second, the data I have used to map out colonial history largely defines the colonial era as ending within the 20th century and moving into a 'postcolonial' era. 
+                   Nevertheless, relationships of exploitation and dependency between nations hugely uneqal in power continue to this day, whether it be through systems of global capitalism or claims on 'terrorities' like Puerto Rico (in the case of the United States).
+                   To define climate colonialism only by an earlier period is insufficient, and so this project only represents a historical starting point for what is a highly urgent contemporary crisis.
+                   Third, if you find any errors or would like to share any constructive advice, please reach out! You can find my email below at my Github repository for this project."),
+                   h5(a("Project Github", href = "https://github.com/winona2468/Gov-50-Final-Project")),
+                   h4("Thank you for reading!")
+                 )
+               )
+           )
 )
 
 
