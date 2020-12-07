e4cdab90c51c8d4bd3284c77ae88e98677350cc1
Winona Guo <winona_guo@college.harvard.edu>
2020-12-07 04:29:42 -0500

    Small edits on About tab.

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -232,20 +232,14 @@ tabPanel(
                       Over the past few centuries, colonialism has violently affected countries across the world. 
                       Today, alongside ongoing relationships of dependence and domination, the effects of colonial encounters persist. 
                       I would like to know if climate risk is among them.
-                      To do so, I have taken four separate datasets: 
-                        on colonial history, countries in Asia and Africa impacted by colonialism, the CO2 emissions of countries, and the projected climate risk of countries."),
+                      To do so, I have taken three separate datasets: 
+                        on colonial history, the CO2 emissions of countries, and the projected climate risk of countries."),
                    h5("First is a 2007 dataset on colonial history from the Harvard Dataverse. 
                       This was put together by the Issue Correlates of War (ICOW) Project, 
                       which tried 'to identify colonial or other dependency relationships for each state over the past two centuries.' 
                       The dataset includes the years in which countries were decolonized, as well as characteristics like the type of independence, whether it was violent, etc."),
                    h5(a("ICOW Colonial History Data", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/5EMETG")),
-                   h5("Next is a 2017 dataset from the Harvard Dataverse which measures characteristics of colonialism specifically in countries in Africa and Asia.
-                      I found this useful because it contains a column specifying the number of years countries were colonized (a variable not found in the previously mentioned dataset).
-                      I was interested in whether there is a correlation between number of years colonized and climate risk, so I combined this data with climate vulnerability data for a graph on the second tab.
-                      It is important to note that this dataset does not capture important regions in the world like Latin America and Europe. 
-                      Nevertheless, it is still interesting to see the results in Asia and Africa, since those regions have been heavily affected by colonialism over time."),
-                   h5(a("Colonialism in Asia and Africa Data", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/UQZFYA")),
-                   h5("Third is a 2011 dataset on climate risk, created by David Wheeler for the Center for Global Development. 
+                   h5("Second is a 2011 dataset on climate risk, created by David Wheeler for the Center for Global Development. 
                       This was perhaps the most important dataset for my project, because it 'measures the vulnerability of 233 countries to three major effects of climate change: extreme weather-related disasters, sea-level rise, and reduced agricultural productivity'.
                       For example, with the extreme weather vulnerability variable, Wheeler writes, 'My core model specifies climate impact risk as a function of radiative forcing from atmospheric accumulation of CO2. I define climate impact risk in year t as the 
                       probability that a representative individual will be affected by an extreme weather event in that year.' The 'vulnerable' variable I use is the overall metric across the three effects of climate change.
@@ -253,13 +247,15 @@ tabPanel(
                       I wonder whether a dataset collected today would reflect significantly different comparative risks across countries.
                       Nevertheless, since this project discusses the effects of the colonial era (especially in the sense of political control, legal status, and physical occupation, all still going strong in the 20th century), perhaps data closer to then is still useful to see."),
                    h5(a("Climate Risk Data", href = "https://www.cgdev.org/publication/dataset-vulnerability-climate-change")),
-                   h5("Last is a 2013 dataset from the Carbon Dioxide Information Analysis Center (Boden, Marland, Andres). 
+                   h5("Third is a 2013 dataset from the Carbon Dioxide Information Analysis Center (Boden, Marland, Andres). 
                       It measures CO2 Emissions from fossil-fuels per country annually from 1751 to 2014.
                       I found this dataset on Github, prepared by the datahub.io project."),
                    h5(a("CO2 Emissions Data", href = "https://github.com/datasets/co2-fossil-by-nation")),
+                   h5("If I were to expand my project, I would consider this 2017 dataset from the Harvard Dataverse which measures characteristics of colonialism in countries in Africa and Asia."),
+                   h5(a("Colonialism in Asia and Africa Data", href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/UQZFYA")),
                    h4("Finally, I just wanted to sound three notes of caution: First, do not interpret any of the linkages I have created on this site as causal. I am interested in examining whether there is a relationship between different variables as a predictive question.
                    Second, the data I have used to map out colonial history largely defines the colonial era as ending within the 20th century and moving into a 'postcolonial' era. 
-                   Nevertheless, relationships of exploitation and dependency between nations hugely uneqal in power continue to this day, whether it be through systems of global capitalism or claims on 'terrorities' like Puerto Rico (in the case of the United States).
+                   Nevertheless, relationships of exploitation and dependency between nations hugely unequal in power continue to this day, whether it be through systems of global capitalism or claims on 'terrorities' like Puerto Rico (in the case of the United States).
                    To define climate colonialism only by an earlier period is insufficient, and so this project only represents a historical starting point for what is a highly urgent contemporary crisis.
                    Third, if you find any errors or would like to share any constructive advice, please reach out! You can find my email below at my Github repository for this project."),
                    h5(a("Project Github", href = "https://github.com/winona2468/Gov-50-Final-Project")),
